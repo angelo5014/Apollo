68a062fa8457a9b73c525066a70a7718c3ae5b8a
Angelo Viana <angelojunior50@yahoo.com.br>
2015-08-25 14:45:42 -0300

    Starting beacon development

--- app/src/main/java/com/app/apollo/UserActivity.java
+++ app/src/main/java/com/app/apollo/UserActivity.java
@@ -3,12 +3,80 @@ package com.app.apollo;
 import android.app.Activity;
 import android.os.Bundle;
 
+    import java.util.Collection;
 
-public class UserActivity extends Activity {
+    import android.app.Activity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.user);
+    import android.os.Bundle;
+    import android.os.RemoteException;
+    import android.util.Log;
+    import android.widget.EditText;
+
+    import org.altbeacon.beacon.AltBeacon;
+    import org.altbeacon.beacon.Beacon;
+    import org.altbeacon.beacon.BeaconConsumer;
+    import org.altbeacon.beacon.BeaconManager;
+    import org.altbeacon.beacon.BeaconParser;
+    import org.altbeacon.beacon.RangeNotifier;
+    import org.altbeacon.beacon.Region;
+
+    public class UserActivity extends Activity implements BeaconConsumer {
+        protected static final String TAG = "RangingActivity";
+        private BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.user);
+
+            beaconManager.bind(this);
+        }
+
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            beaconManager.unbind(this);
+        }
+
+        @Override
+        protected void onPause() {
+            super.onPause();
+            if (beaconManager.isBound(this)) beaconManager.setBackgroundMode(true);
+        }
+
+        @Override
+        protected void onResume() {
+            super.onResume();
+            if (beaconManager.isBound(this)) beaconManager.setBackgroundMode(false);
+        }
+
+        @Override
+        public void onBeaconServiceConnect() {
+            beaconManager.setRangeNotifier(new RangeNotifier() {
+                @Override
+                public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                    if (beacons.size() > 0) {
+                        //EditText editText = (EditText)RangingActivity.this.findViewById(R.id.rangingText);
+                        Beacon firstBeacon = beacons.iterator().next();
+                        logToDisplay("The first beacon " + firstBeacon.toString() + " is about " + firstBeacon.getDistance() + " meters away.");
+                    }
+                }
+
+            });
+
+            try {
+                beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void logToDisplay(final String line) {
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    EditText editText = (EditText)UserActivity.this.findViewById(R.id.rangingText);
+                    editText.append(line+"\n");
+                }
+            });
+        }
     }
-}
